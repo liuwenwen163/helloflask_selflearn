83a7b2324d0d2515b5dfa5cbb03352f512ee2597
liuwenwen163 <liuwenwen163@gamil.com>
2019-09-25 16:43:42 +0800

    temp test

--- chapter01/hello.py
+++ chapter01/hello.py
@@ -4,7 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# the minimal Flask application
-@app.route('/')
-def index():
-    return '<h1>Hello, World!</h1>'
+@app.route('/greet')
+@app.route('/greet/<name>')
+def index(name="programer"):
+    return '<h1>Hello, %s !</h1>' % name
+
